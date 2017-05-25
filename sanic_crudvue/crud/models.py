2432186fc48a6729b395829c2b4ef95881acd42b
顾鲍尔 <gubaoer@QXIT-PC-000087.local>
2017-05-25 21:16:54 +0800

    close #7 custom rest api

--- sanic_crudvue/crud/models.py
+++ sanic_crudvue/crud/models.py
@@ -1,20 +1,34 @@
-from peewee import CharField, DateTimeField, SqliteDatabase, Model, TextField
 import datetime
 
+from peewee import CharField, DateTimeField, SqliteDatabase, Model, TextField
+from playhouse.shortcuts import model_to_dict
 
 db = SqliteDatabase('info.db')
+
 
 class BaseModel(Model):
     class Meta:
         database = db
 
-class ShanghaiPersonInfo(BaseModel):
-    route_url = '/api/shanghai/person'
+    @classmethod
+    def all(cls):
+        data = cls.select().iterator()
+        return [model_to_dict(row) for row in data]
 
+    @classmethod
+    def values_list(cls, *args, **kwargs):
+        result = []
+        for arg in args:
+            qs_expression = "{0}.select({0}.{1}).iterator()".format(cls.__name__, arg)
+            for row in eval(qs_expression):
+                result.append(eval('row.{0}'.format(arg)))
+        return result
+
+
+class ShanghaiPersonInfo(BaseModel):
     create_datetime = DateTimeField(default=datetime.datetime.utcnow(), null=True)
     username = CharField()
     email = CharField()
     phone = CharField()
     sex = CharField()
-    zone =  TextField()
-
+    zone = TextField()
